53953372e5538af0c28323eaa3136adb16ead44b
kayake <75052344+kayake@users.noreply.github.com>
2024-08-06 20:37:34 -0300

    OK, I'm done

--- src/essentials/functions/toLocaleStringFormatter.js
+++ src/essentials/functions/toLocaleStringFormatter.js
@@ -1,8 +1,9 @@
 const fns = {
-  'PT-br': (x) => x.replace(',', '.'), // if there's a comma, such as 10220,92
+  'pt-br': (x) => x.replace(',', '.'), // if there's a comma, such as 10220,92
 }
 
 module.exports = (k, l) => {
+  l ??= "pt-br"
   const n = fns[l](k)
-  return Number(n).toLocaleString(l ?? 'PT-br')
+  return Number(n).toLocaleString(l)
 }
