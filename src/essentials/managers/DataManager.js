ecef62e22bbac1e6d4bb5c1cc936cabcdd095022
kayake <75052344+kayake@users.noreply.github.com>
2024-08-07 08:58:38 -0300

    I made my own JSON database

--- src/essentials/managers/DataManager.js
+++ src/essentials/managers/DataManager.js
@@ -1,1 +1,40 @@
-module.exports = class DataManager {}
+const { writeFileSync, readFileSync } = require('node:fs');
+const path = require('node:path');
+
+module.exports = class DataManager {
+  constructor(file) {
+    this.path = path.resolve(__dirname, file);
+  }
+  
+  read() {
+    return readFileSync(this.path, "utf-8")
+  }
+  
+  write(data) {
+    return writeFileSync(this.path, data, "utf-8")
+  }
+  
+  set(key, value) {
+    const result = this.read()
+    if(!result) return this.write()
+    
+    const oldData = JSON.parse(result)
+    oldData[key] = value
+    
+    const data = JSON.stringify(oldData)
+    return [data, this.write(data)]
+  }
+  
+  delete(data) {
+    const result = this.read()
+    if(!result) return;
+    
+    return [data, delete data]
+  }
+  
+  get_specific_key(key) {
+    const result = this.read()
+    
+    return [key.split('.').reduce((acc, key) => acc[key], result), true]
+  }
+}
