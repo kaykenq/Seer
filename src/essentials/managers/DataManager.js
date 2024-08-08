824065ce7f4343177e52b7230eb4e3dc62542089
kayake <75052344+kayake@users.noreply.github.com>
2024-08-08 13:23:44 -0300

    Nah, never mind 😔

--- src/essentials/managers/DataManager.js
+++ src/essentials/managers/DataManager.js
@@ -1,58 +1,9 @@
 // @ts-check
-const { writeFileSync, readFileSync } = require('node:fs');
+const { JSONFilePreset } = require('lowdb')
 const path = require('node:path');
 
-module.exports = class DataManager {
-  constructor(file) {
-    this.path = path.resolve(__dirname, file);
-  }
-  
-  read() {
-    return readFileSync(this.path, "utf-8")
-  }
-  
-  write(data) {
-    return writeFileSync(this.path, data, "utf-8")
-  }
-  
-  set(key, value) {
-    const result = this.read()
-    if(!result) return this.write()
-    
-    const oldData = JSON.parse(result)
-    oldData[key] = value
-    
-    const data = JSON.stringify(oldData)
-    return [data, this.write(data)]
-  }
-  
-  delete(data) {
-    const result = this.read()
-    if(!result) return;
-    
-    return [data, delete data]
-  }
-  
-  get_specific_key(key) {
-    const result = this.read()
-    
-    return [key.split('.').reduce((acc, key) => acc[key], result), true]
-  }
-  
-  get(key) {
-    const result = this.read()
-    
-    return result[key]
-  }
-  /**
-   * @description Get all Database's Schemes
-   * @returns {object}
-   * @example
-   * ```js
-   * <DataManager>.getAll()
-   * ```
-   */
-  getAll() {
-    return this.read()
+module.exports = class DataManager extends JSONFilePreset {
+  constructor(file, pr) {
+    super(path(__dirname, file), pr || { results: [], })
   }
 }
--- src/fn/downloader.js
+++ src/fn/downloader.js
@@ -1,16 +1,35 @@
 const Request = require("../essentials/HTTP.js");
 const CacheManager = require("../essentials/managers/CacheManager.js");
+const DataManager = require("../essentials/managers/DataManager.js");
+const InitializeClusters = require("../essentials/functions/process.js");
 
 module.exports = class Downloader extends Request {
   constructor(options) {
     super(options)
     
     this.cache = new CacheManager()
+    this.result = new DataManager()
   }
   
   async get_from_specific_id(id) {
-    const r = await make({ resource: id })
+    const r = await super.make({ resource: id })
     
     return this.cache.set(id, r)
+  }
+  
+  async get_all_ids_range(startIndex, endIndex) {
+    const results = []
+    
+    for(let i = startIndex; i <= endIndex; i++) {
+      results.push(this.get_from_specific_id(i).winningNumbers)
+    }
+    
+    Promise.all(ids)
+  }
+  
+  async _start() {
+    const total = (await super.make()).id
+    
+    InitializeClusters(total, this.get_all_ids_range)
   }
 }
